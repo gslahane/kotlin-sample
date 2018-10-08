d5a3c5ca707b4f75f641b8039d8c2464cb316e97
Gangadhar Lahane <shivlahane@outlook.com>
2018-10-08 15:24:53 +0530

    Initial commit

---
+++ src/conditional/maxof.kt
@@ -0,0 +1,12 @@
+package conditional
+
+fun main(args: Array<String>) {
+    val a = 10
+    val b = 20
+
+    if (a > b) {
+        println("A is larger")
+    } else {
+        println("B is larger")
+    }
+}
